5e7987bebb50f22c79eda563ca1c492396de974b
[Ayman Shiham] <sharktank38000@gmail.com>
2024-01-28 20:10:36 -0500

    added functionality to the interest rate slider

---
+++ script.js
@@ -0,0 +1,9 @@
+function compute(){
+    p = document.getElementById("principal").value;
+    
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval
+}
